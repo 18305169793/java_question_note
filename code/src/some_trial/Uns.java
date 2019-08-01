8db44b97157edd574a4dc85447b54e10bbfe7519
mingwenz <mingwen.zang@citrix.com>
2019-08-01 18:04:24 +0800

    add code

---
+++ code/src/some_trial/Uns.java
@@ -0,0 +1,9 @@
+package some_trial;
+import java.util.concurrent.atomic.AtomicInteger;
+public class Uns {
+    public static void main(String[] args) {
+        AtomicInteger ai = new AtomicInteger(0);
+        ai.incrementAndGet();
+        System.out.println(ai);
+    }
+}
